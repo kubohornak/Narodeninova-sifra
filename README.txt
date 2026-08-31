67aa594ee7ed3a9087c882df58d7853e1e75cf3b
kubohornak <kubohornakphoto@gmail.com>
2026-08-31 08:12:51 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+NARODENINOVÁ ŠIFROVAČKA – VERZIA 2
+
+UPRAVUJ HLAVNE config.js.
+Pri každej šifre vieš zmeniť image, password a hint.
+
+Príklad:
+{ image: "images/sifra1.jpg", password: "bratislava", hint: "Pozri sa na čísla ako na písmená abecedy." }
+
+NOVÉ:
+- tlačidlo i s vlastnou nápovedou pri každej šifre,
+- progres sa automaticky ukladá v prehliadači,
+- po 10. hesle sa zobrazí poukážka,
+- tlačidlo Stiahnuť poukážku.
+
+Obrázky vlož do images a názov zmeň v config.js.
+GitHub Pages: nahraj všetky súbory do public repository → Settings → Pages → Deploy from a branch → main → / → Save.
---
+++ config.js
@@ -0,0 +1,11 @@
+const GAME_CONFIG={pageTitle:"Desať šifier. Jeden darček.",eyebrow:"NARODENINOVÁ ŠIFROVAČKA",introText:"Pred tebou je séria desiatich šifier. Každá správna odpoveď odomkne ďalšiu. Keď vyriešiš poslednú, čaká ťa prekvapenie.",finalTitle:"Všetky šifry sú vyriešené.",finalText:"Toto je tvoj narodeninový darček.",voucherImage:"images/poukazka.svg",puzzles:[
+{image:"images/sifra1.svg",password:"heslo1",hint:"Sem napíš nápovedu k prvej šifre."},
+{image:"images/sifra2.svg",password:"heslo2",hint:"Sem napíš nápovedu k druhej šifre."},
+{image:"images/sifra3.svg",password:"heslo3",hint:"Sem napíš nápovedu k tretej šifre."},
+{image:"images/sifra4.svg",password:"heslo4",hint:"Sem napíš nápovedu k štvrtej šifre."},
+{image:"images/sifra5.svg",password:"heslo5",hint:"Sem napíš nápovedu k piatej šifre."},
+{image:"images/sifra6.svg",password:"heslo6",hint:"Sem napíš nápovedu k šiestej šifre."},
+{image:"images/sifra7.svg",password:"heslo7",hint:"Sem napíš nápovedu k siedmej šifre."},
+{image:"images/sifra8.svg",password:"heslo8",hint:"Sem napíš nápovedu k ôsmej šifre."},
+{image:"images/sifra9.svg",password:"heslo9",hint:"Sem napíš nápovedu k deviatej šifre."},
+{image:"images/sifra10.svg",password:"heslo10",hint:"Sem napíš nápovedu k desiatej šifre."}]};
