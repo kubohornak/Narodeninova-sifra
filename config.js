e5126c98fefa3810a32efc9e1d6afd118fd55223
kubohornak <kubohornakphoto@gmail.com>
2026-09-01 18:16:33 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
-const GAME_CONFIG={pageTitle:"Desať šifier. Jeden darček.",eyebrow:"NARODENINOVÁ ŠIFROVAČKA",introText:"Pred tebou je séria desiatich šifier. Každá správna odpoveď odomkne ďalšiu. Keď vyriešiš poslednú, čaká ťa prekvapenie.",finalTitle:"Všetky šifry sú vyriešené.",finalText:"Toto je tvoj narodeninový darček.",voucherImage:"images/poukazka.svg",puzzles:[
-{image:"images/sifra1.svg",password:"heslo1",hint:"Sem napíš nápovedu k prvej šifre."},
-{image:"images/sifra2.svg",password:"heslo2",hint:"Sem napíš nápovedu k druhej šifre."},
-{image:"images/sifra3.svg",password:"heslo3",hint:"Sem napíš nápovedu k tretej šifre."},
-{image:"images/sifra4.svg",password:"heslo4",hint:"Sem napíš nápovedu k štvrtej šifre."},
-{image:"images/sifra5.svg",password:"heslo5",hint:"Sem napíš nápovedu k piatej šifre."},
-{image:"images/sifra6.svg",password:"heslo6",hint:"Sem napíš nápovedu k šiestej šifre."},
-{image:"images/sifra7.svg",password:"heslo7",hint:"Sem napíš nápovedu k siedmej šifre."},
-{image:"images/sifra8.svg",password:"heslo8",hint:"Sem napíš nápovedu k ôsmej šifre."},
-{image:"images/sifra9.svg",password:"heslo9",hint:"Sem napíš nápovedu k deviatej šifre."},
-{image:"images/sifra10.svg",password:"heslo10",hint:"Sem napíš nápovedu k desiatej šifre."}]};
+const GAME_CONFIG={pageTitle:"Desať šifier. Jeden darček.",eyebrow:"NARODENINOVÁ ŠIFROVAČKA",introText:"Pred tebou je séria desiatich šifier. Každá správna odpoveď odomkne ďalšiu. Keď vyriešiš poslednú, čaká ťa prekvapenie.",finalTitle:"Všetky šifry sú vyriešené.",finalText:"Toto je tvoj narodeninový darček.",voucherImage:"images/poukazka.jpg",puzzles:[
+{image:"images/sifra1.jpg",password:"Narodeniny",hint:"Tu bude vždy nápoveda"},
+{image:"images/sifra2.jpg",password:"Utorok",hint:"Toto fakt zvládneš aj bez nápovedy"},
+{image:"images/sifra3.jpg",password:"Tyrsak",hint:"Všetko je to vec perspektývy"},
+{image:"images/sifra4.jpg",password:"Pokemon",hint:"Toľko peňazí som 17 rokov nevidel"},
+{image:"images/sifra5.jpg",password:"Vecernicek",hint:"Každého z týchto delikventov si sledovala pri práci"},
+{image:"images/sifra6.jpeg",password:"Bar",hint:"Tu stačia iba tri písmená"},
+{image:"images/sifra7.jpg",password:"Hana",hint:"STARÉ dobré časy"},
+{image:"images/sifra8.png",password:"Koruna",hint:"Není to zajímavé ako sa niektoré solvá na nič nepodobajú?"},
+{image:"images/sifra9.jpg",password:"Qg6+Qxg7#",hint:"Táto vínimka není slovo, je to hatmatilka a obsahuje aj znak #"},
+{image:"images/sifra10.jpg",password:"Kozmonaut",hint:"Ideálne na komunikáciu o polnohospodárstve"}
+]};
