ad2eed91bdcba208ecbd43fe84d846d5ea5c535d
kubohornak <kubohornakphoto@gmail.com>
2026-09-01 18:38:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-const GAME_CONFIG={pageTitle:"Všetko Najlepšie Hana",eyebrow:"NARODENINOVÁ ŠIFROVAČKA",introText:"Pred tebou je séria šifier. Každá správna odpoveď odomkne ďalšiu. Keď vyriešiš poslednú, čaká ťa prekvapenie.",finalTitle:"Všetky šifry sú vyriešené.",finalText:"Toto je tvoj narodeninový darček.",voucherImage:"images/poukazka.jpg",puzzles:[
+const GAME_CONFIG={pageTitle:"VŠETKO NAJLEPŠIE HANA",eyebrow:"NARODENINOVÁ ŠIFROVAČKA",introText:"Pred tebou je séria šifier. Každá správna odpoveď odomkne ďalšiu. Keď vyriešiš poslednú, čaká ťa prekvapenie.",finalTitle:"Všetky šifry sú vyriešené.",finalText:"Toto je tvoj narodeninový darček.",voucherImage:"images/poukazka.jpg",puzzles:[
 {image:"images/sifra1.jpg",password:"Narodeniny",hint:"Tu bude vždy nápoveda"},
 {image:"images/sifra2.jpg",password:"Utorok",hint:"Toto fakt zvládneš aj bez nápovedy"},
-{image:"images/sifra3.jpg",password:"Tyrsak",hint:"Všetko je to vec perspektývy"},
+{image:"images/sifra3.jpg",password:"Tyrsak",hint:"Všetko je to vec perspektívy"},
 {image:"images/sifra4.jpg",password:"Pokemon",hint:"Toľko peňazí som 17 rokov nevidel"},
 {image:"images/sifra5.jpg",password:"Vecernicek",hint:"Každého z týchto delikventov si sledovala pri práci"},
 {image:"images/sifra6.jpeg",password:"Bar",hint:"Tu stačia iba tri písmená"},
 {image:"images/sifra7.jpg",password:"Hana",hint:"STARÉ dobré časy"},
 {image:"images/sifra8.png",password:"Koruna",hint:"Není to zajímavé ako sa niektoré solvá na nič nepodobajú?"},
-{image:"images/sifra9.jpg",password:"Qg6+Qxg7#",hint:"Táto vínimka není slovo, je to hatmatilka a obsahuje aj znak #"},
+{image:"images/sifra9.jpg",password:"Qg6+Qxg7#",hint:"Táto vínimka není slovo, je to hatmatilka a obsahuje aj znak + a #"},
 {image:"images/sifra10.jpg",password:"Kozmonaut",hint:"Ideálne na komunikáciu o polnohospodárstve"}
 ]};
